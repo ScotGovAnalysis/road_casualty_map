e096ca7154127aa523901a449e1e1691dd839f46
Ivet Gazova <ivet.gazova@gmail.com>
2023-11-08 15:04:56 +0000

    fixing render code

--- R/03_Render_Rmarkdown.R
+++ R/03_Render_Rmarkdown.R
@@ -1,7 +1,10 @@
 ### this code renders the Rmarkdown file 
 ### please run this code 
 
+library(rmarkdown)
+library(here)
+
 render(
-  input = "Rmarkdown.Rmd",
-  output_file = "Casualty_map.html"
+  input = here("R","Rmarkdown.Rmd"),
+  output_file = here ("R", "Casualty_map.html")
 )
