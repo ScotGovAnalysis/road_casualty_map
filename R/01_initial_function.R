7aaa56e8aa8e02845ab556ac79bcf2dbe382c262
Caldwell K (Katrina) <Katrina.Caldwell@transport.gov.scot>
2024-08-20 17:39:08 +0100

    trying to set code up to make five maps for last five years - issue in intial function script running

--- R/01_initial_function.R
+++ R/01_initial_function.R
@@ -13,7 +13,6 @@ snapcastem <- read.csv(here("data", "snapcastem.csv"))
 snapcasacc <- merge(snapcastem, snapacctem, by.x = "ACCREF", by.y = "ACCREF", all.x = TRUE)
 
 #filter casualties based on year selected, convert coordinates to long/lat
-
 casualties <- snapcasacc %>%  
   select(YEAR.x, CASAGE, Council, EAST, NORTH, RUSER, CASSEV) %>% 
   mutate(road_user = case_when(
@@ -67,87 +66,4 @@ casualties_fatal_5 <- casualties %>%
 
 casualties_fatal_serious_5 <- casualties %>%
   filter(CASSEV %in% c(1,2), YEAR.x == mapyear-4) 
-
-# casualties_categories <- casualties %>%
-#   mutate(road_user = case_when(
-#     road_user == 'Pedestrian' ~ 'Pedestrians',
-#     road_user == 'Pedal cycle' ~ 'Pedal cyclists',
-#     road_user == 'Motorcycle' ~ 'Motorcyclists',
-#     road_user == 'Car' ~ 'Cars and taxis',
-#     road_user == 'Taxi' ~ 'Cars and taxis',
-#     road_user == 'Minibus' ~ 'Bus, Coach, Minibus',
-#     road_user == 'Bus/coach' ~ 'Bus, Coach, Minibus',
-#     road_user == 'LGV' ~ 'LGV and HGV',
-#     road_user == 'HGV' ~ 'LGV and HGV',
-#     road_user == 'Other' ~ 'Other'
-#   ))
-
-##select for all the different groups for easier manipulation for leaflet
-#
-# fatals <- casualties %>% 
-#   filter(CASSEV == 1)
-# 
-# fatalities_ped <- casualties %>% 
-#   filter(CASSEV == 1 & RUSER == 1)
-# 
-# serious_ped <- casualties %>% 
-#   filter(CASSEV == 2 & RUSER == 1)
-# 
-# fatalities_cyc <- casualties %>% 
-#   filter(CASSEV == 1 & RUSER == 2)
-# 
-# serious_cyc <- casualties %>% 
-#   filter(CASSEV == 2 & RUSER == 2)
-# 
-# fatalities_moto <- casualties %>% 
-#   filter(CASSEV == 1 & RUSER == 3)
-# 
-# serious_moto <- casualties %>% 
-#   filter(CASSEV == 2 & RUSER == 3)
-# 
-# fatalities_car <- casualties %>% 
-#   filter(CASSEV == 1 & (RUSER == 4 | RUSER == 5) )
-# 
-# serious_car <- casualties %>% 
-#   filter(CASSEV == 2 & (RUSER == 4 | RUSER == 5) )
-# 
-# fatalities_bus <- casualties %>% 
-#   filter(CASSEV == 1 & (RUSER == 6 | RUSER == 7) )
-# 
-# serious_bus <- casualties %>% 
-#   filter(CASSEV == 2 & (RUSER == 6 | RUSER == 7) )
-# 
-# fatalities_lor <- casualties %>% 
-#   filter(CASSEV == 1 & (RUSER == 8 | RUSER == 9) )
-# 
-# serious_lor <- casualties %>% 
-#   filter(CASSEV == 2 & (RUSER == 8 | RUSER == 9) )
-# 
-# fatalities_other <- casualties %>% 
-#   filter(CASSEV == 1 & RUSER == 10)
-# 
-# serious_other <- casualties %>% 
-#   filter(CASSEV == 2 & RUSER == 10)
-# 
-# ped <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & RUSER == 1)
-# 
-# cyc <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & RUSER == 2)
-# 
-# moto <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & RUSER == 3)
-# 
-# car <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & (RUSER == 4 | RUSER == 5))
-# 
-# bus <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & (RUSER == 6 | RUSER == 7))
-# 
-# lor <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & (RUSER == 8 | RUSER == 9))
-# 
-# other <- casualties %>% 
-#   filter((CASSEV == 1 | CASSEV == 2)  & RUSER == 10)
-
 
--- R/03_Render_Rmarkdown.R
+++ R/03_Render_Rmarkdown.R
@@ -5,6 +5,26 @@ library(rmarkdown)
 library(here)
 
 render(
-  input = here("R","Rmarkdown.Rmd"),
+  input = here("R","Rmarkdown1.Rmd"),
   output_file = here ("R", "Casualty_map.html")
+)
+
+render(
+  input = here("R","Rmarkdown2.Rmd"),
+  output_file = here ("R", "Casualty_map_2022.html")
+)
+
+render(
+  input = here("R","Rmarkdown3.Rmd"),
+  output_file = here ("R", "Casualty_map_2022.html")
+)
+
+render(
+  input = here("R","Rmarkdown4.Rmd"),
+  output_file = here ("R", "Casualty_map_2020.html")
+)
+
+render(
+  input = here("R","Rmarkdown5.Rmd"),
+  output_file = here ("R", "Casualty_map_2019.html")
 )
